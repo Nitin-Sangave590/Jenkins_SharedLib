a4acf5aece4e17ffc4fec7c32f5032d196389e04
Nitin Sangave <nitinsangave995@gmail.com>
2025-12-29 22:00:02 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,16 @@
-def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
-      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
-  }
-  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+// def call(String Project, String ImageTag, String dockerhubuser){
+//   withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
+//       sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+//   }
+//   sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+// }
+
+def call(String project, String imageTag, String dockerHubUser) {
+    withCredentials([string(credentialsId: 'dockerhub-token', variable: 'DOCKERHUB_TOKEN')]) {
+        sh '''
+          echo "$DOCKERHUB_TOKEN" | docker login -u ''' + dockerHubUser + ''' --password-stdin
+          docker push ''' + dockerHubUser + '''/''' + project + ''':'''+ imageTag + '''
+        '''
+    }
 }
+
