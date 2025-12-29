c0daa3ddff4bb82d69d8ab3c83ffd5efef524e8e
Nitin Sangave <nitinsangave995@gmail.com>
2025-12-29 22:00:18 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,9 @@
-// Define function
-def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+// // Define function
+// def call(String ProjectName, String ImageTag, String DockerHubUser){
+//   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+// }
+
+
+def call(String projectName, String imageTag, String dockerHubUser) {
+    sh "docker build -t ${dockerHubUser}/${projectName}:${imageTag} ."
 }
